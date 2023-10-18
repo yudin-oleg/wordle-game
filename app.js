5e4c0ea0d20594414edc6cccea55b3cfdffd73ac
Oleg <yudin_123@mail.ru>
2023-10-18 19:30:59 +0300

    feat: add functions to message about victory, lose or mistake

--- app.js
+++ app.js
@@ -73,16 +73,18 @@ function enterWord(){
     }
     //chech if user entered less than 5 letters then show a message about it and don't allow to press the buttons
     else if(letterNumber < 5){
+        messageTooShort();
         return;
     }
-    
+
     //get a word entered by a user
     var gamerWord = getGamerWord();
 
     //check if a word entered by a user exists if it does not exist do not process it
     if(!doesUsersWordExists(gamerWord)){
-        alert("This word does not exist!");
-                return;
+        messageDoNotExist();
+        // alert("This word does not exist!");
+        return;
     }
     //check what status do letters from uers word have
     for(let i=0; i<5; i++){
@@ -107,6 +109,7 @@ function enterWord(){
     }
     letterNumber = 0;
     attemptNumber++;
+    reactToUserWonLostTried(gamerWord);
 }
 
 //
@@ -130,4 +133,101 @@ function doesUsersWordExists(userWord){
     else{
         return false;
     }
+}
+
+//function to react when user lost or won the game and to disable letter buttons for
+//the time of animation which starts after user clicked the "enter" button
+function reactToUserWonLostTried(gamerWord){
+    var gamerWord = gamerWord;
+    //check if user word equals the planned one then message anout victory and end the game
+    //set timeout to allow changing color animation to end before showing a message 
+    if(gamerWord == word){
+        attemptNumber = 6;
+        setTimeout(() => {
+            messageVictoryLose("Congratulations! You won! Try new game!");
+        }, 500);
+    }
+    //check if all attempts were used and the riddle wasn't solved then message about the lose
+    //set timeout to allow changing color animation to end before showing a message 
+    else if(attemptNumber === 6){
+        setTimeout(() => {
+            messageVictoryLose(`You lost. The word was ${word.toLowerCase()}. Try again.`);
+        }, 500);
+    }
+    //disable letters in the alphabet/letter buttons until animation stops to work
+    //set timeout to allow changing color animation to end before allowimg enter a new word 
+    else{
+        for (let i = 0; i< letterButtons.length; i++){
+            letterButtons[i].setAttribute("disabled", "");
+        }
+        setTimeout(() => {
+            for (let i = 0; i< letterButtons.length; i++){
+                letterButtons[i].removeAttribute("disabled");
+            }
+        }, 500);
+    }
+}
+
+//
+//function to show a message that entered word is too short
+function messageTooShort(){
+    for (let i = 0; i< letterButtons.length; i++){
+        letterButtons[i].setAttribute("disabled", "");
+    }
+    for (let i = 0; i< actionButtons.length; i++){
+        actionButtons[i].setAttribute("disabled", "");
+    }
+    document.getElementById("tooShortMessage").classList.add("showMessage");
+    setTimeout(() => {
+        document.getElementById("tooShortMessage").classList.remove("showMessage");
+        for (let i = 0; i< letterButtons.length; i++){
+            letterButtons[i].removeAttribute("disabled");
+        }
+        for (let i = 0; i< actionButtons.length; i++){
+            actionButtons[i].removeAttribute("disabled");
+        }
+    }, 1000);
+}
+
+//
+//function to show a message that entered word doesn't exist
+function messageDoNotExist(){
+    for (let i = 0; i< letterButtons.length; i++){
+        letterButtons[i].setAttribute("disabled", "");
+    }
+    for (let i = 0; i< actionButtons.length; i++){
+        actionButtons[i].setAttribute("disabled", "");
+    }
+    document.getElementById("doNotExistMessage").classList.add("showMessage");
+    setTimeout(() => {
+        document.getElementById("doNotExistMessage").classList.remove("showMessage");
+        for (let i = 0; i< letterButtons.length; i++){
+            letterButtons[i].removeAttribute("disabled");
+        }
+        for (let i = 0; i< actionButtons.length; i++){
+            actionButtons[i].removeAttribute("disabled");
+        }
+    }, 1000);
+}
+
+//
+//function to show a message about victory or lose
+function messageVictoryLose(message){
+    var message = message;
+    document.getElementById("victoryLoseMessage").childNodes[0].textContent = message;
+    document.getElementById("victoryLoseMessage").classList.add("showMessage");
+}
+
+//
+//function to go back to the page
+function goBack(){
+    document.getElementById("victoryLoseMessage").classList.remove("showMessage");
+    document.getElementById("giveUpButton").classList.remove("showMessage");
+    document.getElementById("newGameButton").classList.add("showMessage");
+}
+
+//
+//function to reload the page
+function reload(){
+    window.location.reload();
 }
